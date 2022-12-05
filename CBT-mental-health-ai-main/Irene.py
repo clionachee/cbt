1dd0dd010e2b44f395c64fbfaae0124cadb1c169
Cliona Chee <94758696+clionachee@users.noreply.github.com>
2022-12-06 05:34:54 +0800

    Update Irene.py

--- CBT-mental-health-ai-main/Irene.py
+++ CBT-mental-health-ai-main/Irene.py
@@ -1,31 +1,40 @@
+import json
+
 import streamlit as st
 from streamlit_chat import message
 from conversant.prompts import ChatPrompt
 import conversant
 import cohere
 
+import requests #pip intall requests
+from streamlit_lottie import st_lottie #install streamlit-lottie-for-front-end-animation
+
+from embeddify import Embedder
+
 
 # Internal variables
-COHERE_API_KEY = "API KEY"
+COHERE_API_KEY = "Aa3yKEwtz0wRbRRuPTZ6VvPqXrS9Nvgn9uh6cawn"
 
+#------------I-Rene's Back-End---------
 co = cohere.Client(COHERE_API_KEY)
-
-
-if 'bot' not in st.session_state:
-    st.session_state['bot'] = conversant.PromptChatbot.from_persona("therapist", client=co)
-
+bot = conversant.PromptChatbot.from_persona("therapist", client=co)
+# page defaults
 st.set_page_config(
     page_title="Irene",
-    page_icon="🧙"
+    page_icon=":mage:"
 )
 
+
+# check: 'key' already exists in session_state
+# if not existing: initialize it
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
     
+# Session State also supports the attribute based syntax
     st.session_state['inputs'] = []
 
 # top header variables
-HEAD_DESC = "I-rene is a chatbot specializing in Cognitive Behavioral Therapy (CBT), your first line of help when you have no access to therapy. \n CBT is a form of psychological treatment that has been demonstrated to be effective for a range of problems including depression, anxiety disorders, alcohol and drug use problems, marital problems, eating disorders, and severe mental illness. Numerous research studies suggest that CBT leads to significant improvement in functioning and quality of life. See more [here](https://www.apa.org/ptsd-guideline/patients-and-families/cognitive-behavioral). \n I-Rene will give you different exercises for your needs and encourage you to keep exercising for better mental health."
+HEAD_DESC = "I-rene is a chatbot specializing in Cognitive Behavioral Therapy (CBT), your first line of help when you have no access to therapy. CBT is ... More info on CBT - button. \n I-Rene will give you different exercises for your needs and encourage you to keep exercising for better mental health."
 
 # top header
 header_cont = st.container()
@@ -42,8 +51,27 @@ def get_input():
 
 user_input = get_input()
 st.session_state.inputs.append(user_input)
-st.session_state.generated.append(st.session_state.bot.reply(user_input))
+st.session_state.generated.append(bot.reply(user_input))
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i), avatar_style="initials",seed="Irene")
-        message(st.session_state['inputs'][i], is_user=True, key=str(i) + '_user',seed="user",avatar_style="initials")
+        message(st.session_state['inputs'][i], is_user=True, key=str(i) + '_user',seed="user",avatar_style="initials") 
+#------------I-Rene's Back-End---------
+
+#------------Claiming I-Rene's CBT Healer MintKudos NFT---------
+if st.checkbox("Claim Healing NFT with free mint and gas fees are not charged to you!"):
+    "https://mintkudos.xyz/claim/2a1dfa46-c0fc-431d-b4f8-1ae2ddc5bb4f"
+#------------Claiming MintKudos NFT---------
+
+#------------I-Rene's Animation for Front-End---------
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status.code != 200:
+        return None
+    return r.json()
+#------------I-Rene's Animation for Front-End---------
+
+#------------Loading I-Rene's Animation for Front-End---------
+    
+lottie_hello = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_rMOSF5k88d.json")
+#------------Loading I-Rene's Animation for Front-End---------
